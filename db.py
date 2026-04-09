d27d2aa90b57145bd5a145e00e16d07ac63b854d
kartikwagh002 <Kartikwagh2008@gmail.com>
2026-04-09 20:58:51 +0530

    prepare for deployment

--- db.py
+++ db.py
@@ -1,10 +1,10 @@
+import os
 import mysql.connector
 
 def get_db_connection():
-    conn = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="root123",
-        database="chronofinance"
+    return mysql.connector.connect(
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME")
     )
-    return conn
